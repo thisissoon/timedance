cf48420a1fd348e0b8c8989e69060ed602e3a453
kennethshaw <kennethshaw@KennethShawsMBP.lan>
2017-03-19 18:15:56 +0000

    adjusted page transition

--- static/js/animation.js
+++ static/js/animation.js
@@ -1,5 +1,11 @@
 $( document ).ready(function() {
     $("article.release-item .image").each(function(i) {
-		$(this).delay(i * 60).fadeIn(600);
+		$(this).delay(i * 40).fadeIn(1200);
+	});
+});
+
+$( document ).ready(function() {
+    $("article.event-item .image").each(function(i) {
+		$(this).delay(i * 40).fadeIn(1200);
 	});
 });
