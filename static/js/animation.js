08521c16b0d0a4096d545edd8009ecc5861e73c1
kennethshaw <kennethshaw@KennethShawsMBP.lan>
2017-06-11 14:08:07 +0100

    Toggle function

--- static/js/animation.js
+++ static/js/animation.js
@@ -1,13 +1,22 @@
-/*
-$( document ).ready(function() {
-    $("article.release-item .image").each(function(i) {
-		$(this).delay(i * 40).fadeIn(1200);
-	});
-});
 
-$( document ).ready(function() {
-    $("article.event-item .image").each(function(i) {
-		$(this).delay(i * 40).fadeIn(1200);
-	});
-});
-*/
+// $( document ).ready(function() {
+//     $("article.release-item .image").each(function(i) {
+// 		$(this).delay(i * 40).fadeIn(1200);
+// 	});
+// });
+
+// $( document ).ready(function() {
+//     $("article.event-item .image").each(function(i) {
+// 		$(this).delay(i * 40).fadeIn(1200);
+// 	});
+// });
+
+
+// function myFunction() {
+//     var x = document.getElementById('myDIV');
+//     if (x.style.display === 'none') {
+//         x.style.display = 'block';
+//     } else {
+//         x.style.display = 'none';
+//     }
+// }
